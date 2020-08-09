b806a2d7ae5aed418405e2309f3f44ab152f34c7
Daniel Yim <55238464+tootboi@users.noreply.github.com>
2020-08-09 17:02:34 +1200

    implemented convolution matrix for image sharpening

--- imageProcess.js
+++ imageProcess.js
@@ -1,9 +1,9 @@
-let stream, processed, context, temp, tempContext, viewWidth, viewHeight;
+let stream, ogCanvas, context, temp, tempContext, viewWidth, viewHeight, frame;
 
 function init() {
     stream = document.getElementById('video');
-    processed = document.getElementById('processed');
-    context = processed.getContext('2d');
+    ogCanvas = document.getElementById('processed');
+    context = ogCanvas.getContext('2d');
 
     temp = document.createElement('canvas');
     temp.setAttribute('width', viewWidth);
@@ -15,18 +15,28 @@ function init() {
 
 function computeFrame() {
     tempContext.drawImage(stream, 0, 0, viewWidth, viewHeight);
-    let frame = tempContext.getImageData(0, 0, viewWidth, viewHeight);
+    frame = tempContext.getImageData(0, 0, viewWidth, viewHeight);
 
     //brightness
     let data = frame.data;
     //brightness level
     let level = Number(document.getElementById('brightness').value);
-    console.log(level);
     for (var i=0; i<data.length; i+=4) {
         data[i] += level;       //r
         data[i + 1] += level;   //g
         data[i +  2] += level;  //b
     }
+
+    let divisor = 1;
+    let offset = 0;
+    let sharpen = [[0, -1,  0],[-1,  5, -1],[0, -1,  0]];
+    let edgeDetect = [[0, 1, 0], [1, -4, 1], [0, 1, 0]];
+    let emboss = [[-2, 1, 0], [-1, 1, 1], [0, 1, 2]];
+    let blur = [[.1, .1, .1], [.1, .1, .1], [.1, .1, .1]];
+    let antiAlias = [[0, 1,  0],[1,  2, 1],[0, 1,  0]];
+
+    //sharpening
+    frame = convolve(sharpen, divisor, offset);
 
     context.putImageData(frame, 0, 0);
     //calls itself
@@ -46,8 +56,52 @@ window.addEventListener('resize', () => {
 function resize() {
     viewWidth = window.innerWidth / 2;
     viewHeight = (viewWidth / 16) * 9;
-    processed.setAttribute('width', viewWidth);
-    processed.setAttribute('height', viewHeight);
+    ogCanvas.setAttribute('width', viewWidth);
+    ogCanvas.setAttribute('height', viewHeight);
     temp.setAttribute('width', viewWidth);
     temp.setAttribute('height', viewHeight);
+    video.setAttribute('width', viewWidth);
+    video.setAttribute('height', viewHeight);
 };
+
+function convolve(matrix, divisor, offset) {
+    let m = [].concat(matrix[0], matrix[1], matrix[2]);
+    // if(!divisor) {
+    //     divisor = m.reduce(function(a, b) {return a+b}) || 1;
+    // }
+    let olddata = frame;
+    let oldpx = olddata.data;
+    let newdata = context.createImageData(olddata);
+    let newpx = newdata.data;
+    let len = newpx.length;
+    let res = 0;
+    let w = frame.width;
+
+    for (var i=0; i<len; i++) {
+        if ((i+1) % 4 === 0) {
+            newpx[i] = oldpx[i];
+            continue;
+        };
+        res = 0;
+        let these = [
+            oldpx[i - w * 4 - 4] || oldpx[i],
+            oldpx[i - w * 4]     || oldpx[i],
+            oldpx[i - w * 4 + 4] || oldpx[i],
+            oldpx[i - 4]         || oldpx[i],
+            oldpx[i],
+            oldpx[i + 4]         || oldpx[i],
+            oldpx[i + w * 4 - 4] || oldpx[i],
+            oldpx[i + w * 4]     || oldpx[i],
+            oldpx[i + w * 4 + 4] || oldpx[i]
+        ];
+        for (var j=0; j < 9; j++) {
+            res += these[j] * m[j];
+        }
+        res /= divisor;
+        if(offset) {
+            res += offset;
+        }
+        newpx[i] = res;
+    }
+    return newdata;
+}
